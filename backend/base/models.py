e1366cb5ff6f5657da9f1ba535b369c34a00c795
jv-maroto <javidominguez060@gmail.com>
2024-07-24 08:08:45 +0100

    Contenido Portal con el login backend hecho

---
+++ backend/base/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='profile')
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    dni = models.CharField(max_length=9)
+
+    def __str__(self):
+        return self.user.username
