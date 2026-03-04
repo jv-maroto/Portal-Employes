06eb8e141ecf25980c381ffa802c974d7846ab61
Javier Maroto <javidominguez060@gmail.com>
2026-03-04 10:19:42 +0000

    Actualizar API views

--- backend/base/api/views.py
+++ backend/base/api/views.py
@@ -2,7 +2,7 @@ from rest_framework.response import Response
 from django.contrib.auth.models import User
 from rest_framework.decorators import api_view, permission_classes, parser_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
-from base.models import Post, PostView, PdfFile, Vacacion
+from base.models import Post, PostView, PdfFile, Vacacion, Profile
 from rest_framework import status
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
