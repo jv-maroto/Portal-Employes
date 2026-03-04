1bc5cde3528aa2f217ee37c920fe4f62fe0e85d5
Javier Maroto <javidominguez060@gmail.com>
2026-03-04 09:15:20 +0000

    Actualizar configuración Django

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -224,3 +224,13 @@ EMAIL_USE_TLS = get_env_bool('EMAIL_USE_TLS', default=True)
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', '')
+
+# Seguridad adicional en producción
+if not DEBUG:
+    SECURE_SSL_REDIRECT = True
+    SECURE_HSTS_SECONDS = 31536000
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SECURE_BROWSER_XSS_FILTER = True
