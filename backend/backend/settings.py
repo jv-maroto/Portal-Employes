45472ce7bd18254e68f952f34475b44fa2c2a8bf
Javier Maroto <javidominguez060@gmail.com>
2026-03-04 09:51:12 +0000

    Actualizar configuración Django

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -182,7 +182,16 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '20/minute',
+        'user': '100/minute',
+        'login': '5/minute',
+    }
 }
 
 SIMPLE_JWT = {
