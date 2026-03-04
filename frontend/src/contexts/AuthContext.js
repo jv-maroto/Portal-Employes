761964a7da5bb1b8c975caabdaced4dbb868350d
Javier Maroto <javidominguez060@gmail.com>
2026-03-04 10:24:07 +0000

    Actualizar contexto AuthContext

--- frontend/src/contexts/AuthContext.js
+++ frontend/src/contexts/AuthContext.js
@@ -16,7 +16,8 @@ export const AuthProvider = ({ children }) => {
 
     let loginUser = async (e) => {
         e.preventDefault();
-        const response = await fetch('http://127.0.0.1:8000/api/token/', {
+        const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:8000/api/';
+        const response = await fetch(`${apiUrl}token/`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
